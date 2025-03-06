9133b337e90dd1bb2582b53a14110b2f6e12214e
mysticclub <seishironagi.rukasproyect@gmail.com>
2025-03-06 15:21:37 -0500

    Actualizar info-newcommand.js

--- plugins/info-newcommand.js
+++ plugins/info-newcommand.js
@@ -15,7 +15,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['sugerencia']
 handler.tags = ['owner']
-handler.command = ['sug', 'newcomand', 'report']
+handler.command = ['sug', 'newcomand', 'report', 'suggest']
 handler.group = true;
 
 export default handler
