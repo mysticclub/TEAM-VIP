5529f1d3d8e3b8a12d5e60ad638c71e86efe8c7d
mysticclub <seishironagi.rukasproyect@gmail.com>
2025-03-07 21:47:34 -0500

    Actualizar descargas-instagram.js

--- plugins/descargas-instagram.js
+++ plugins/descargas-instagram.js
@@ -1,29 +1,30 @@
-import { igdl } from "ruhend-scraper"
+import { igdl } from 'ruhend-scraper';
 
-let handler = async (m, { args, conn }) => { 
-if (!args[0]) {
-return conn.reply(m.chat, '⌨︎ *Ingresa un link de Instagram*', m, rcanal)}
-try {
-await m.react(rwait)
-conn.reply(m.chat, `➪ *Enviando El Video...*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: wm,
-previewType: 0, thumbnail: icons,
-sourceUrl: channel }}})      
-let res = await igdl(args[0])
-let data = res.data       
-for (let media of data) {
-await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '➪ *Tu video de instagram.*\n' + textbot, fkontak)
-}} catch {
-await m.react(error)
-conn.reply(m.chat, '⚠︎ Ocurrió un error.', m, rcanal)}}
+const handler = async (m, { args, conn }) => {
+  if (!args[0]) {
+    return conn.reply(m.chat, `${emoji} Por favor, ingresa un enlace de Instagram.`, m);
+  }
 
-handler.command = ['instagram', 'ig']
-handler.tags = ['descargas']
-handler.help = ['instagram', 'ig']
+  try {
+    await m.react(rwait);
+    const res = await igdl(args[0]);
+    const data = res.data;
+
+    for (let media of data) {
+      await conn.sendFile(m.chat, media.url, 'instagram.mp4', `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`, m);
+    await m.react(done);
+    }
+  } catch (e) {
+    return conn.reply(m.chat, `${msm} Ocurrió un error.`, m);
+    await m.react(error);
+  }
+};
+
+handler.command = ['instagram', 'ig'];
+handler.tags = ['descargas'];
+handler.help = ['instagram', 'ig'];
 handler.group = true;
-handler.register = false
+handler.register = true;
+handler.coin = 2;
 
-export default handler
+export default handler;
